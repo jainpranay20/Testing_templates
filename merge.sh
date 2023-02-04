ab04efb8e474d3d7815a8a1fc1ecaab1fea3ea63
jainpranay20 <jainpranay20@gmail.com>
2023-02-04 22:48:33 +0530

    merge

--- merge.sh
+++ merge.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Check if jq is installed
+if ! command -v jq &>/dev/null; then
+  echo "jq is not installed. Installing now..."
+  sudo apt-get install jq
+fi
+
+# Continue with the rest of the script that uses jq
+# ...
+
 echo "Enter the name of the first JSON file: "
 read file1
 
@@ -11,10 +20,11 @@ length=${#file1}
 # Check if both files exist
 if [ -f "$file1" ] && [ -f "$file2" ]; then
     # Create a new file with the name "merged.json"
-    filename=${file1::length-3}
+    filename=${file1::length-8}
 
     # Merge the contents of the two files into the new file
-    cat "$file1" "$file2" > "$filename"
+    jq -s add "$file1" "$file2" > "$filename.json"
+
 
     echo "Files merged into $filename"
 else
